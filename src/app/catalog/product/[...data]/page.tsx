d861fa5ef88aeff06af5bc3508370ab11ee9ec92
Gabriel02ps <gabriel02ps@gmail.com>
2023-11-06 21:54:24 -0300

    React Server Components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ProducProps {
   params: {
     data: string[]
@@ -7,11 +9,19 @@ interface ProducProps {
 export default function Product({params}: ProducProps) {
   const [productId, size, color] = params.data
 
+  console.log(params);
+
+  function addToCart() {
+    console.log(`Adicionou: ${productId}, ${size}, ${color}`) 
+  }
+
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
